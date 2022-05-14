75ad7c2571cb904efe77739c57d5e9fda718424d
umesh <umesharma.us11@gmail.com>
2022-05-14 16:28:05 +0530

    final commit

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 // initial value
-let count = 0
+let count = 0;
 
 // Select value and buttons
+const value = document.getElementById("value");
+const btns = document.querySelectorAll(".btn");
+
+
+btns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+            value.style.color ="red"
+        } else if (styles.contains("increase")) {
+            count++;
+            value.style.color ="green"
+        } else {
+            count = 0;
+         
+        }
+
+        if (count === 0) {
+              value.style.color ="#222" 
+        }
+        value.textContent = count;
+    })
+} )
